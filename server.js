246e4a1ec6fe7d3c5e504acf91120641ef84a606
natebecker11 <natebecker11@gmail.com>
2018-12-23 13:41:56 -0500

    server route to query NYT

--- server.js
+++ server.js
@@ -17,9 +17,33 @@ app.use(express.static('./dist'))
 const databaseUrl = process.env.MONGODB_URI || 'mongodb://localhost/nyt-scraper-react'
 mongoose.connect(databaseUrl, { useNewUrlParser: true })
 
+const nytSearchUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=${process.env.NYT_API_KEY}`
+
 
 
 // route to perform a NYT article search
 app.post('/api/search/', (req, res) => {
-  axios.get()
+  const { begin, end, term } = req.body
+  const queryURL = `${nytSearchUrl}&q=${term}&begin_date=${begin}&end_date=${end}`
+  axios.get(queryURL)
+    .then(({ data }) => {
+
+      // console.log(data.response.docs)
+      const formatted = data.response.docs.map(doc => {
+        return {
+          title: doc.headline.main,
+          author: doc.byline.original.slice(3),
+          description: doc.snippet,
+          link: doc.web_url,
+          publishedDate: doc.pub_date.slice(0, 10),
+        }
+      })
+      res.json(formatted)
+      // res.json(results)
+    })
+    .catch(x => console.log(x))
 })
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT);
