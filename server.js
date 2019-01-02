c10f3e96f878f79810bfc2321034c1df65bf0fd3
natebecker11 <natebecker11@gmail.com>
2019-01-02 16:40:46 -0500

    tweaked for testing

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
-// app.use(express.static('./dist'))
+app.use(express.static(path.join(__dirname, '/client/build')))
 
 
 // const databaseUrl = process.env.MONGODB_URI || 'mongodb://localhost/nyt-scraper-react'
