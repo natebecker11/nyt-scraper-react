fde8f30a9c556b80fe2693d48484d0be14657414
natebecker11 <natebecker11@gmail.com>
2019-01-02 16:31:04 -0500

    reorganized file structure

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.post('/api/search/', (req, res) => {
 
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/build/index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
